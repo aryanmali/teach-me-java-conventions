ec94668fbc2687f9e270915edd3262bba0bfe3b7
Aryan Mali <aryanrvmali@gmail.com>
2022-02-12 01:49:26 +0530

    package resolved and cleaned code

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
 public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+    public static int findPower(int base, int index) {
+        int res = 1;
+        for (int i = 0; i<index; i++) {
+            res *= base;
         }
-        return p;
+        return res;
     }
 }
